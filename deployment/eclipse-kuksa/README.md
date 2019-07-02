39280848d457a9c44105a4c0c5d76c8c4e886f79
Sebastian Lohmeier (INST-CSS/BSV-OS1) <Sebastian.Lohmeier@bosch-si.com>
2019-07-02 20:15:04 +0200

    #56 Add deployment for Kuksa appstore
    Signed-off-by: Sebastian Lohmeier <Sebastian.Lohmeier@bosch-si.com>

--- deployment/eclipse-kuksa/README.md
+++ deployment/eclipse-kuksa/README.md
@@ -12,6 +12,53 @@ https://www.eclipse.org/org/documents/epl-2.0/index.php
 
 ---
 
+## Build Eclipse Kuksa Appstore
+
+* Script:
+  * `build_kuksa_appstore.sh`
+* Purpose:
+  * Build a Docker image for the Eclipse Kuksa Appstore and push it to a Docker registry
+* Options:
+  * `DOCKER_REGISTRY_SECRET`: Name of the secret to access to custom Docker registry. **Note that the secret is created during the build and deploy process within the namespace `extension`.**
+  * `DOCKER_REGISTRY_SERVER`: Address of the Docker registry server, preferrably running on Microsoft Azure.
+  * `DOCKER_REGISTRY_USERNAME`: Username to sign in to the Docker registry.
+  * `DOCKER_REGISTRY_PASSWORD`: Password to sign in to the Docker registry.
+  * `DOCKER_REGISTRY_EMAIL`: Email to sign in to the Docker registry.
+* Stages:
+  * Clone the Kuksa Git repository
+  * Build with Maven
+  * Build Docker image
+  * Push Docker image to Docker registry
+  * Final cleanup
+    * Remove the clones Git repository
+
+
+## Deploy Eclipse Kuksa Appstore
+
+* Script:
+  * `deploy_kuksa_appstore.sh`
+* Purpose:
+  * Deploys a Docker image for the Eclipse Kuksa Appstore on a Kubernetes cluster
+* Options:
+  * `APPSTORE_USERNAME`: The username to set for the admin user of the appstore
+  * `APPSTORE_PASSWORD`: The password to set for the admin user of the appstore
+  * `HAWKBIT_HOST`: The (cluster-internal) hostname of the hawkbit server to use in the appstore 
+  * `HAWKBIT_PORT`: The (cluster-internal) port number of the hawkbit server to use in the appstore
+  * `HAWKBIT_USERNAME`: The username to be used by the appstore to authenticate with
+    hawkbit.
+  * `HAWKBIT_PASSWORD`: The password to be used by the appstore to authenticate with 
+    hawkbit.
+  * `DOCKER_REGISTRY_SECRET`: Name of the secret to access to custom Docker registry. **Note that the secret is created during the build and deploy process within the namespace `extension`.**
+  * `DOCKER_REGISTRY_SERVER`: Address of the Docker registry server, preferrably running on Microsoft Azure.
+  * `DOCKER_REGISTRY_USERNAME`: Username to sign in to the Docker registry.
+  * `DOCKER_REGISTRY_PASSWORD`: Password to sign in to the Docker registry.
+  * `DOCKER_REGISTRY_EMAIL`: Email to sign in to the Docker registry.
+* Notes:
+  * Production mode is enabled for Vaadin.
+  * H2 console is disabled.
+  * The h2 database is persisted via a persistent volume claim.
+  * The service uses a ClusterIP so it is only available behind the Ambassador gateway.
+
 ## Deploy Traccar Server
 
 * Script:
@@ -23,6 +70,7 @@ https://www.eclipse.org/org/documents/epl-2.0/index.php
     * Replace relevant parts in the Kubernetes resource files in a new `target` folder
   * Deployment
     * Create the `kuksa` namespace, or re-use the existing one
+    * Create Docker registry secret, or re-use the existing one
     * Deploy the Kubernetes resources
   * Final cleanup
     * Remove the `target` folder
---
+++ deployment/eclipse-kuksa/deploy_kuksa_appstore.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+######################################################################
+# Copyright (c) 2019 Bosch Software Innovations GmbH [and others]
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+########################################################################
+
+# Make the script fail if a command fails
+set -e
+
+SCRIPTPATH=$(dirname "$(readlink -f "$0")")
+
+APPSTORE_USERNAME=$1
+APPSTORE_PASSWORD=$2
+HAWKBIT_URL=$3
+HAWKBIT_USERNAME=$4
+HAWKBIT_PASSWORD=$5
+DOCKER_REGISTRY_SECRET=$6
+DOCKER_REGISTRY_SERVER=$7
+DOCKER_REGISTRY_USERNAME=$8
+DOCKER_REGISTRY_PASSWORD=$9
+DOCKER_REGISTRY_EMAIL=${10}
+
+VAADIN_PRODUCTION_MODE_ENABLED=true
+
+NAMESPACE=kuksa
+DOCKER_IMAGE_NAME="kuksa-appstore"
+VERSION="0.0.1-SNAPSHOT"
+
+TARGET_DIR=$SCRIPTPATH/kuksa-appstore/target
+APPLICATION_YML_TEMPLATE=$SCRIPTPATH/kuksa-appstore/application.yml
+APPLICATION_YML=$TARGET_DIR/application.yml
+PVC_DESCRIPTOR_TEMPLATE=$SCRIPTPATH/kuksa-appstore/kuksa-appstore-pvc.yaml
+PVC_DESCRIPTOR=$TARGET_DIR/kuksa-appstore-pvc.yaml
+SECRET_DESCRIPTOR_TEMPLATE=$SCRIPTPATH/kuksa-appstore/kuksa-appstore-secret.yaml
+SECRET_DESCRIPTOR=$TARGET_DIR/kuksa-appstore-secret.yaml
+DEPLOYMENT_DESCRIPTOR_TEMPLATE=$SCRIPTPATH/kuksa-appstore/kuksa-appstore-deployment.yaml
+DEPLOYMENT_DESCRIPTOR=$TARGET_DIR/kuksa-appstore-deployment.yaml
+SERVICE_DESCRIPTOR_TEMPLATE=$SCRIPTPATH/kuksa-appstore/kuksa-appstore-service.yaml
+SERVICE_DESCRIPTOR=$TARGET_DIR/kuksa-appstore-service.yaml
+
+. $SCRIPTPATH/../kubernetes/includes/allIncludes.inc
+
+echo
+echo "##############################################################"
+echo "##############################################################"
+echo "############### Eclipse Kuksa Cloud deployment ###############"
+echo "##############################################################"
+echo "##############################################################"
+
+echo
+echo "########## Initialization ##########"
+
+cd $SCRIPTPATH
+
+echo 
+echo "##### Customize configuration #####"
+mkdir -p $TARGET_DIR
+cp $APPLICATION_YML_TEMPLATE $APPLICATION_YML
+sed -i "s/<APPSTORE_USERNAME>/$APPSTORE_USERNAME/" $TARGET_DIR/application.yml
+sed -i "s/<APPSTORE_PASSWORD>/$APPSTORE_PASSWORD/" $TARGET_DIR/application.yml
+sed -i "s#<HAWKBIT_URL>#$HAWKBIT_URL#" $TARGET_DIR/application.yml
+sed -i "s/<HAWKBIT_USERNAME>/$HAWKBIT_USERNAME/" $TARGET_DIR/application.yml
+sed -i "s/<HAWKBIT_PASSWORD>/$HAWKBIT_PASSWORD/" $TARGET_DIR/application.yml
+sed -i "s/<VAADIN_PRODUCTION_MODE_ENABLED>/$VAADIN_PRODUCTION_MODE_ENABLED/" $TARGET_DIR/application.yml
+
+cp $PVC_DESCRIPTOR_TEMPLATE $PVC_DESCRIPTOR
+sed -i "s/<VERSION>/$VERSION/" $PVC_DESCRIPTOR 
+
+cp $SECRET_DESCRIPTOR_TEMPLATE $SECRET_DESCRIPTOR
+APPLICATION_YML_BASE64="$(cat $APPLICATION_YML | base64 --wrap=0)"
+sed -i "s/<APPLICATION_YML_BASE64>/$APPLICATION_YML_BASE64/" $SECRET_DESCRIPTOR
+
+cp $DEPLOYMENT_DESCRIPTOR_TEMPLATE $DEPLOYMENT_DESCRIPTOR
+sed -i "s/<VERSION>/$VERSION/" $DEPLOYMENT_DESCRIPTOR
+sed -i "s/<DOCKER_REGISTRY_SERVER>/$DOCKER_REGISTRY_SERVER/" $DEPLOYMENT_DESCRIPTOR
+sed -i "s/<DOCKER_IMAGE_NAME>/$DOCKER_IMAGE_NAME/" $DEPLOYMENT_DESCRIPTOR
+sed -i "s/<DOCKER_REGISTRY_SECRET>/$DOCKER_REGISTRY_SECRET/" $DEPLOYMENT_DESCRIPTOR
+
+cp $SERVICE_DESCRIPTOR_TEMPLATE $SERVICE_DESCRIPTOR
+sed -i "s/<VERSION>/$VERSION/" $SERVICE_DESCRIPTOR
+
+echo
+echo "##### Create namespace #####"
+createOrReuseNamespace $NAMESPACE
+
+echo
+echo "##### Create secret #####"
+createOrReuseDockerRegistrySecret \
+	$DOCKER_REGISTRY_SECRET \
+	$NAMESPACE \
+	$DOCKER_REGISTRY_SERVER \
+	$DOCKER_REGISTRY_USERNAME \
+	$DOCKER_REGISTRY_PASSWORD \
+	$DOCKER_REGISTRY_EMAIL
+
+echo
+echo "##### Deploy docker image #####"
+kubectl apply -f $PVC_DESCRIPTOR -n $NAMESPACE
+kubectl apply -f $SECRET_DESCRIPTOR -n $NAMESPACE
+kubectl apply -f $DEPLOYMENT_DESCRIPTOR -n $NAMESPACE
+kubectl apply -f $SERVICE_DESCRIPTOR -n $NAMESPACE
+
+echo
+echo  "########## Final cleanup ##########"
+
+cd $SCRIPTPATH
+ls -lah
+
+echo
+echo "##### Delete kuksa folder #####"
+
+rm -rf $TARGET_DIR
