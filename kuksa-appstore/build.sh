1373437accf9a7bb0247cf9bb2f68ea483cd4d3c
Sven Erik Jeroschewski <44381577+eriksven@users.noreply.github.com>
2019-08-14 10:38:45 +0200

    separate build and deployment for App-Store (#102)
    Signed-off-by: Sven Erik Jeroschewski <svenerik.jeroschewski@bosch-si.com>

---
+++ kuksa-appstore/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+######################################################################
+# Copyright (c) 2019 Bosch Software Innovations GmbH [and others]
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+########################################################################
+
+# Make the script fail if a command fails
+set -e
+
+echo
+echo "##############################################################"
+echo "##############################################################"
+echo "############ Eclipse Kuksa Cloud app-store build #############"
+echo "##############################################################"
+echo "##############################################################"
+
+echo
+echo "########## Initialization ##########"
+
+echo
+echo "##### Build app-store with Maven #####"
+
+mvn clean verify
+
+# Needs the xq command, see https://github.com/kislyuk/xq
+VERSION="$(xq .project.version pom.xml | tr -d '"')"
+cp target/kuksa.appstore-$VERSION.jar kuksa-appstore.jar
