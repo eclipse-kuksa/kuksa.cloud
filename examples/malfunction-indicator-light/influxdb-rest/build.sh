751642cf8677e6c001996dc7b5cdc0559a359b5a
Sven Erik Jeroschewski <svenerik.jeroschewski@bosch-si.com>
2019-09-06 18:16:52 +0200

    added build and pushImage scripts for the mil use case
    Signed-off-by: Sven Erik Jeroschewski <svenerik.jeroschewski@bosch-si.com>

---
+++ examples/malfunction-indicator-light/influxdb-rest/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+######################################################################
+# Copyright (c) 2019 Bosch Software Innovations GmbH [and others]
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+########################################################################
+
+# Make the script fail if a command fails
+set -e 
+
+VERSION="$(gradle properties -q | grep 'version:' | awk '{print $2}')"
+
+echo
+echo "##############################################################"
+echo "##############################################################"
+echo "############# Eclipse Kuksa InfluxDB-Rest build ##############"
+echo "##############################################################"
+echo "##############################################################"
+
+
+echo
+echo "##### Build with Gradle #####"
+gradle clean bootJar
+
+cp build/libs/influxdb-rest-${VERSION}.jar influxdb-rest.jar
